710a6348f9e469d7318b85cf17d54c4c1229bcc6
Tu Nombre <tuemail@example.com>
2025-12-04 19:25:47 -0300

    actualizacion

--- Parcial/settings.py
+++ Parcial/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,16 +91,17 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-
-
 STATIC_URL = "/static/"
+
 STATICFILES_DIRS = [
-    Path(__file__).resolve().parent.parent / "static",
+    BASE_DIR / "static",   
 ]
 
-
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
